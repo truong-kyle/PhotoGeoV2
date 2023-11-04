e6c211959ccafe3f98c46b5a49153b5a0dc619e8
Kyle Truong <kyle.trng@gmail.com>
2023-11-04 19:28:32 -0400

    Add Help page and resources

--- public/source.js
+++ public/source.js
@@ -2,41 +2,40 @@ let view, firemap, map, firemode;
 firemode = document.getElementById("flame");
 firemode.style.display = "none";
 
-  window.onload = fetch('/apis/arcgis')
-    .then(response => response.json())
-    .then(data => {
-        initMap(data.arcgisApiKey);
-        toggleFire();
-    })
-    .catch(error => console.error('Error fetching API key:', error));
+window.onload = fetch("/apis/arcgis")
+  .then((response) => response.json())
+  .then((data) => {
+    initMap(data.arcgisApiKey);
+    toggleFire();
+  })
+  .catch((error) => console.error("Error fetching API key:", error));
 
-    document
-    .getElementById("imageInput")
-    .addEventListener("change", function (event) {
-      const file = event.target.files[0];
-      if (file) {
-        const reader = new FileReader();
-  
-        reader.onload = function (e) {
-          const imagePreview = document.getElementById("imagePreview");
-          imagePreview.src = e.target.result;
-          imagePreview.style.display = "block";
-        };
-  
-        reader.readAsDataURL(file);
-      }
-    });
+document
+  .getElementById("imageInput")
+  .addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
 
-function initMap(api){
-  require(["esri/config", "esri/WebMap", "esri/Map", "esri/views/MapView"], function (
-    esriConfig,
-    WebMap,
-    Map,
-    MapView
-  ) {
-  
+      reader.onload = function (e) {
+        const imagePreview = document.getElementById("imagePreview");
+        imagePreview.src = e.target.result;
+        imagePreview.style.display = "block";
+      };
+
+      reader.readAsDataURL(file);
+    }
+  });
+
+function initMap(api) {
+  require([
+    "esri/config",
+    "esri/WebMap",
+    "esri/Map",
+    "esri/views/MapView",
+  ], function (esriConfig, WebMap, Map, MapView) {
     esriConfig.apiKey = api;
-  
+
     firemap = new WebMap({
       portalItem: {
         // id for webmap
@@ -45,9 +44,9 @@ function initMap(api){
     });
 
     blankmap = new Map({
-      basemap: "arcgis/topographic"
-    })
-  
+      basemap: "arcgis/topographic",
+    });
+
     view = new MapView({
       map: firemap,
       center: [-79.41866, 43.678352], // Longitude, latitude
@@ -57,20 +56,22 @@ function initMap(api){
   });
 }
 
-function toggleFire(){
-  if (firemode.style.display == "none"){
+function toggleFire() {
+  if (firemode.style.display == "none") {
     firemode.style.display = "inline-block";
-    document.getElementById("clickme").style.color = 'orange';
+    document.getElementById("clickme").style.color = "orange";
+    document.getElementById("clickme").title = "Click to switch to Normal Mode";
     view.map = firemap;
-    document.getElementById("uploadButton").textContent="Search for Hotspots"
-    document.getElementById("clickme").textContent = "Wildfire Mode"
-  }
-  else{
+    document.getElementById("uploadButton").textContent = "Search for Hotspots";
+    document.getElementById("clickme").textContent = "Wildfire Mode";
+  } else {
     firemode.style.display = "none";
     view.map = blankmap;
-    document.getElementById("uploadButton").textContent="Upload to Photo Map"
-    document.getElementById("clickme").textContent = "Normal Mode"
-    document.getElementById("clickme").style.color = "lightblue"
+    document.getElementById("uploadButton").textContent = "Upload to Photo Map";
+    document.getElementById("clickme").textContent = "Normal Mode";
+    document.getElementById("clickme").style.color = "lightblue";
+    document.getElementById("clickme").title =
+      "Click to switch to Wildfire Mode";
   }
 }
 
@@ -92,16 +93,20 @@ function uploadImage() {
     .then((response) => response.json())
     .then((data) => {
       document.getElementById("result").textContent = data.message;
-      if (firemode.style.display == "inline-block"){
+      if (firemode.style.display == "inline-block") {
         checkFires();
+      } else {
+        fetch("/getloc")
+          .then((response) => response.json())
+          .then((data) => {
+            updateMap(data.lat, data.lon);
+            document.getElementById(
+              "fireResult"
+            ).textContent = `Photo taken at ${data.lat.toFixed(
+              5
+            )}, ${data.lon.toFixed(5)}`;
+          });
       }
-      else {
-        fetch("/getloc").then((response) => response.json()).then(data =>{
-          updateMap(data.lat, data.lon);
-          document.getElementById("fireResult").textContent = `Photo taken at ${data.lat.toFixed(5)}, ${data.lon.toFixed(5)}`;
-        })
-      }
-     
     })
     .catch((error) => {
       console.error("Error:", error);
@@ -110,14 +115,38 @@ function uploadImage() {
     });
 }
 
-function checkFires(){
-  fetch("/checkfire").then((response) => response.json()).then(data => {
-    if(data.dataLat && data.dataLon){
-      updateMap(data.dataLat, data.dataLon);
-      document.getElementById("fireResult").textContent = `Fire detected near hotspot ${data.dataLat}, ${data.dataLon}`
-    }
-    else{
-      document.getElementById("fireResult").textContent = data.message;
-    }
-  });
+function checkFires() {
+  fetch("/checkfire")
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.dataLat && data.dataLon) {
+        updateMap(data.dataLat, data.dataLon);
+        document.getElementById(
+          "fireResult"
+        ).textContent = `Fire detected near hotspot ${data.dataLat}, ${data.dataLon}`;
+      } else {
+        document.getElementById("fireResult").textContent = data.message;
+      }
+    });
+}
+
+function helpOff() {
+  document.getElementById("help").style.display = "none";
+}
+
+function helpOn() {
+  document.getElementById("help").style.display = "block";
+}
+
+function openMenu() {
+  if (document.getElementById("menuList").style.visibility =="visible"){
+    document.getElementById("menuList").style.visibility = "hidden"
+    document.getElementById("menuList").style.opacity = 0
+    document.getElementById("menuIcon").style.opacity = 1
+  }
+  else {
+    document.getElementById("menuList").style.visibility = "visible"
+    document.getElementById("menuIcon").style.opacity = 0.5
+    document.getElementById("menuList").style.opacity = 1
+  }
 }
